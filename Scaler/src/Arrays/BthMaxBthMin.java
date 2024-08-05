9d4adf63c928a17ee8b459b7c4cc469429d9c140
Mohamed Safi <safiizz24.gmail.com>
2024-08-05 16:15:51 +0530

    Day 7: Array 4

---
+++ Scaler/src/Arrays/BthMaxBthMin.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BthMaxBthMin {
+    public static void main(String[] args) {
+        BthMaxBthMin maxMin = new BthMaxBthMin();
+        ArrayList<Integer> aList = new ArrayList<>();
+        aList.add(5);
+        aList.add(17);
+        aList.add(100);
+        aList.add(11);
+
+        int B = 1;
+        int result = maxMin.solve(aList, B);
+        System.out.println(result);
+    }
+
+    public int solve(ArrayList<Integer> A, int B) {
+        int N = A.size();
+        Collections.sort(A);
+        int BthMax = A.get(N - B);
+        int BthMin = A.get(B - 1);
+        return BthMax - BthMin;
+    }
+}
---
+++ Scaler/src/Arrays/MinMaxInArray.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class MinMaxInArray {
+    public static void main(String[] args) {
+        MinMaxInArray minmax = new MinMaxInArray();
+        ArrayList<Integer> aList = new ArrayList<>();
+        //[-2, 1, -4, 5, 3]
+        aList.add(-2);
+        aList.add(1);
+        aList.add(-4);
+        aList.add(5);
+        aList.add(3);
+
+        int result = minmax.solve(aList);
+        System.out.println(result);
+    }
+
+    public int solve(ArrayList<Integer> A) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        int N = A.size();
+        for (int i = 0; i < N; i++) {
+            if (A.get(i) > max) {
+                max = A.get(i);
+            }
+            if (A.get(i) < min) {
+                min = A.get(i);
+            }
+        }
+        return max + min;
+    }
+}
